f8b9504aeee527cd007848eae1c024dc90293fb0
irvan <irvanirawanit@gmail.com>
2023-07-06 14:20:48 +0700

    tes

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"runtime"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Machine/Server Information:")
+	fmt.Println("===========================")
+
+	// Retrieve and print the operating system details
+	fmt.Printf("Operating System: %s\n", runtime.GOOS)
+	fmt.Printf("Architecture: %s\n", runtime.GOARCH)
+
+	// Retrieve and print the host name
+	host, err := os.Hostname()
+	if err == nil {
+		fmt.Printf("Host Name: %s\n", host)
+	}
+
+	// Retrieve and print the number of CPUs
+	numCPU := runtime.NumCPU()
+	fmt.Printf("Number of CPUs: %d\n", numCPU)
+
+	// Retrieve and print the Goroutine count
+	numGoroutine := runtime.NumGoroutine()
+	fmt.Printf("Number of Goroutines: %d\n", numGoroutine)
+
+	// Retrieve and print memory statistics
+	var memStats runtime.MemStats
+	runtime.ReadMemStats(&memStats)
+	fmt.Printf("Allocated Memory: %.2f MB\n", float64(memStats.Alloc)/(1024*1024))
+	fmt.Printf("Total Allocated Memory: %.2f MB\n", float64(memStats.TotalAlloc)/(1024*1024))
+	fmt.Printf("Heap Memory: %.2f MB\n", float64(memStats.HeapAlloc)/(1024*1024))
+	fmt.Printf("Heap Objects: %d\n", memStats.HeapObjects)
+
+	// Retrieve and print environment variables
+	envVars := os.Environ()
+	fmt.Println("\nEnvironment Variables:")
+	fmt.Println("======================")
+	for _, envVar := range envVars {
+		envVarParts := strings.SplitN(envVar, "=", 2)
+		fmt.Printf("%s = %s\n", envVarParts[0], envVarParts[1])
+	}
+}
